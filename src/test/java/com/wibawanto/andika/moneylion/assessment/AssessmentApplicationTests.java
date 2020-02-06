a5c3e379e4a5383d574402b1502d940ef5ca3233
Andika <andika@11street.my>
2020-02-06 16:10:28 +0800

    Initial commit

---
+++ src/test/java/com/wibawanto/andika/moneylion/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wibawanto.andika.moneylion.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
