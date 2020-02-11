040b76e8c56f05a779cc105d6443d1f06060e263
Andika <andika@11street.my>
2020-02-11 16:46:47 +0800

    update dependencies

--- src/main/java/com/wibawanto/andika/moneylion/assessment/AssessmentApplication.java
+++ src/main/java/com/wibawanto/andika/moneylion/assessment/AssessmentApplication.java
@@ -2,6 +2,10 @@ package com.wibawanto.andika.moneylion.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class AssessmentApplication {
@@ -9,5 +13,22 @@ public class AssessmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AssessmentApplication.class, args);
 	}
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
 
 }
